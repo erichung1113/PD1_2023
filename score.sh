4165d019d0c765387d3dbafe4abe1ec753e13f13
RUEI TING HUNG <101823195+erichung1113@users.noreply.github.com>
2023-10-03 02:42:46 -0500

    add a function to test single user

--- score.sh
+++ score.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
-# To Do
-# -lm
-
 # change here
 HW=2
-ToDo="A B C D"
+ProblemID="A B C D"
 Score=(30 10 30 30)
 # -----------
 
@@ -16,22 +13,27 @@ cp -r /share/HW$HW/* ~/Homework/testcase
 score_file="HW${HW}_score.txt"
 WA_file="HW${HW}_wa.txt"
 
+students_id=$(cat ~/students_id.txt)
+if (( $# != 0 )); then
+	students_id=$@
+fi
+
 echo "id,score" > $score_file
 echo "id,score" > $WA_file
 
-for student_id in $(cat ~/students_id.txt); do 
+for student_id in $students_id; do 
+    echo -n $student_id
+    
     score=0
     if test -d $student_id/HW$HW ; then
         if ! test -d $student_id/HW$HW/your_answer ; then mkdir $student_id/HW$HW/your_answer; fi 
         
-        echo "testing $student_id"
-        
-        for p in $ToDo; do
+        for p in $ProblemID; do
             if ! test -f $student_id/HW$HW/p$p.c ; then continue; fi
             if ! test -d $student_id/HW$HW/your_answer/p$p ; then mkdir $student_id/HW$HW/your_answer/p$p; fi
             
             if test -f $student_id/HW$HW/$p ; then rm $student_id/HW$HW/$p; fi 
-            gcc -w $student_id/HW$HW/p$p.c -o $student_id/HW$HW/$p -lm 2> /dev/null 
+            gcc -w $student_id/HW$HW/p$p.c -o $student_id/HW$HW/$p 2> /dev/null 
             if (( $? != 0 )); then continue; fi
             
             tcNum=0
@@ -45,18 +47,22 @@ for student_id in $(cat ~/students_id.txt); do
                 timeout 1 $student_id/HW$HW/$p < $input > $result
                 if (( $? != 0 )); then continue; fi
             
-                ./tester $p $result $answer
-                if (( $? == 1 )); then (( AC += 1 )) ; fi
+                diff $result $answer >> /dev/null
+                if (( $? == 0 )); then (( AC += 1 )) ; fi
 
-                # diff $result $answer >> /dev/null
-                # if (( $? == 0 )); then (( AC += 1 )) ; fi
+                # ./tester $p $result $answer
+                # if (( $? == 1 )); then (( AC += 1 )) ; fi
             done
+
             if (( $AC == $tcNum )); then
                 (( score += ${Score[(( $(printf "%d" "'$p") - 65 ))]} ))
-            else
+            	echo -n ", p$p:${Score[(( $(printf "%d" "'$p") - 65 ))]}" 
+			else
                 echo "$student_id,$p" >> $WA_file
+            	echo -n ", p$p: 0"
             fi
         done
     fi
     echo "$student_id,$score" >> $score_file
+    echo ", total:$score"
 done
