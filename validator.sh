b78424998804ee805582c2333317a64ba71d4863
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2023-10-11 22:02:40 -0500

    add TLE/CE/RE

--- validator.sh
+++ validator.sh
@@ -26,23 +26,37 @@ for p in $ToDo; do
     if ! test -d ~/HW$HW/your_answer/p$p ; then mkdir ~/HW$HW/your_answer/p$p; fi
     if test -f ~/HW$HW/$p ; then rm ~/HW$HW/$p; fi 
     g++ ~/HW$HW/p$p.c -o ~/HW$HW/$p
-	if (( $? != 0 )); then continue; fi
+	if (( $? != 0 )); then 
+		echo -e "${YELLOW}Compilation Error${RESET}"
+  		echo "-----------------------------"
+		continue; 
+	fi
 
     for tc_name in $(ls ~/HW$HW/testcase/p$p | grep 'in$' | sed 's/.in//'); do
         input=~/HW$HW/testcase/p$p/$tc_name.in
         answer=~/HW$HW/testcase/p$p/$tc_name.out
         result=~/HW$HW/your_answer/p$p/$tc_name.out
         echo -n "$tc_name : "
-        ~/HW$HW/$p < $input > $result
-        diff $result $answer >> /dev/null
-        if (( $? != 0 )); then
-            echo -e "${RED}WA${RESET}"
-            echo "Input   Data   : $(cat $input)" 
-            echo "Your    Answer : $(cat $result)" 
-            echo "Correct Answer : $(cat $answer)" 
-        else
-            echo -e "${GREEN}AC${RESET}"
-        fi
+		ExecResult=$(timeout 1 bash -c "~/HW$HW/$p < $input > $result" 2>&1)
+		ExecReturnValue=$?
+		if (( $ExecReturnValue != 0 )); then 
+	    	if (( $ExecReturnValue == 124 )); then
+				echo -e "${BLUE}Time Limit Exceed${RESET}"
+			else
+				echo -e "${YELLOW}Runtime Error${RESET}"
+				echo $ExecResult		
+   			fi
+		else
+	        diff $result $answer >> /dev/null
+	        if (( $? != 0 )); then
+	            echo -e "${RED}WA${RESET}"
+	            echo "Input   Data   : $(cat $input)" 
+	            echo "Your    Answer : $(cat $result)" 
+	            echo "Correct Answer : $(cat $answer)" 
+	        else
+	            echo -e "${GREEN}AC${RESET}"
+	        fi
+		fi
         echo "-----------------------------"
     done
 done
