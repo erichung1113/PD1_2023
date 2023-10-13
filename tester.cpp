57a46c0c6499b996868be5c5f2764a90c97f217b
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2023-10-13 01:42:53 -0500

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -4,27 +4,20 @@ using namespace std;
 #define int long long
 
 signed main(signed argc, char *argv[]){
-    ifstream result(argv[2]);
+    ifstream userans(argv[2]);
     ifstream answer(argv[3]);
 
-    vector<string> ans,res;
-    string line;
-    while(getline(answer,line)) if(line != "") ans.push_back(line);
-    while(getline(result,line)) if(line != "") res.push_back(line);
-    
     if(argv[1][0] == 'A'){
-        if(res.size() != 1) return 0;
-        int a,b,d,e;
-        char c;
-        stringstream resultss(res[0]);
-        stringstream answerss(ans[0]);
-        resultss >> a >> c >> b;
-        answerss >> d >> c >> e;
-        int g1=gcd(a,b);
-        int g2=gcd(d,e);
-        if(a/g1==d/g2 && b/g1==e/g2) return 1;
-        else return 0;
+        vector<string> ans,uans;
+        string line;
+        while(getline(answer,line)) ans.push_back(line);
+        while(getline(userans,line)) uans.push_back(line);
+        return (ans == uans);
     }else{
-        return (ans == res);
+        double ans, uans;
+        answer >> ans;
+        userans >> uans;
+        if(abs(ans-uans)<=1e-12) return 1;
+        else return 0;
     }
 }
