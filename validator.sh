aee1bf98569f0f7aef5f776a3cde913d739adaf6
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2023-10-16 16:08:23 -0500

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -5,8 +5,14 @@ HW=4
 ToDo="A B"
 # -------------
 
+FilePath=~/HW$HW
+
 if (( $# != 0 )); then
-    ToDo=$@
+    if [[ $1 =~ /* ]]; then
+        FilePath=$1
+    else
+        ToDo=$@
+    fi
 fi
 
 RED='\033[0;31m'
@@ -15,29 +21,29 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
 
-if ! test -d ~/HW$HW/your_answer ; then mkdir ~/HW$HW/your_answer; fi
-if test -d ~/HW$HW/testcase ; then rm -r ~/HW$HW/testcase; fi 
-mkdir ~/HW$HW/testcase
-cp -r /share/HW$HW/* ~/HW$HW/testcase
+if ! test -d $FilePath/your_answer ; then mkdir $FilePath/your_answer; fi
+if test -d $FilePath/testcase ; then rm -r $FilePath/testcase; fi 
+mkdir $FilePath/testcase
+cp -r /share/HW$HW/* $FilePath/testcase
 
 for p in $ToDo; do
     echo -e "${YELLOW}Testing p$p.c ...${RESET}\n-----------------------------"
 
-    if ! test -d ~/HW$HW/your_answer/p$p ; then mkdir ~/HW$HW/your_answer/p$p; fi
-    if test -f ~/HW$HW/$p ; then rm ~/HW$HW/$p; fi 
-    gcc ~/HW$HW/p$p.c -o ~/HW$HW/$p
+    if ! test -d $FilePath/your_answer/p$p ; then mkdir $FilePath/your_answer/p$p; fi
+    if test -f $FilePath/$p ; then rm $FilePath/$p; fi 
+    gcc $FilePath/p$p.c -o $FilePath/$p
     if (( $? != 0 )); then 
         echo -e "${YELLOW}Compilation Error${RESET}"
         echo "-----------------------------"
         continue; 
     fi
 
-    for tc_name in $(ls ~/HW$HW/testcase/p$p | grep 'in$' | sed 's/.in//'); do
-        input=~/HW$HW/testcase/p$p/$tc_name.in
-        answer=~/HW$HW/testcase/p$p/$tc_name.out
-        result=~/HW$HW/your_answer/p$p/$tc_name.out
+    for tc_name in $(ls $FilePath/testcase/p$p | grep 'in$' | sed 's/.in//'); do
+        input=$FilePath/testcase/p$p/$tc_name.in
+        answer=$FilePath/testcase/p$p/$tc_name.out
+        result=$FilePath/your_answer/p$p/$tc_name.out
         echo -n "$tc_name : "
-        ExecResult=$(timeout 1 bash -c "~/HW$HW/$p < $input > $result" 2>&1)
+        ExecResult=$(timeout 1 bash -c "$FilePath/$p < $input > $result" 2>&1)
         ExecReturnValue=$?
         if (( $ExecReturnValue != 0 )); then 
             if (( $ExecReturnValue == 124 )); then
@@ -51,24 +57,10 @@ for p in $ToDo; do
             /usr/local/bin/hw4_tester $p $result $answer
             if (( $? == 0 )); then
                 echo -e "${RED}WA${RESET}"
-                if (( $( wc -l < $input) + 1 > 1));then
-                    echo -e "Input   Data   : \n$(cat $input)"
-                else
-                    echo "Input   Data   : $(cat $input)" 
-                fi
-
-                if (( $( wc -l < $result) + 1 > 1));then
-                    echo -e "Your    Answer : \n$(cat $result)"
-                else
-                    echo "Your    Answer : $(cat $result)" 
-                fi
-                
-                if (( $( wc -l < $answer) + 1 > 1));then
-                    echo -e "Correct Answer : \n$(cat $answer)"
-                else
-                    echo "Correct Answer : $(cat $answer)" 
-                fi
-
+	    	echo -e "${BLUE}Input   Data${RESET}   : \n$(cat $input)"
+	    	echo -e "${BLUE}Your    Answer${RESET} : \n$(cat $result)"
+	    	echo -e "${BLUE}Correct Answer${RESET} : \n$(cat $answer)"
+            echo -e "${YELLOW}Note: You can use the following command to compare your code's output with the TA's answer :${RESET}\ndiff ~/HW$HW/your_answer/p$p/$tc_name.out ~/HW$HW/testcase/p$p/$tc_name.out" 
             else    
                 echo -e "${GREEN}AC${RESET}"
             fi
