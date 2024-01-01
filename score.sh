60b05d4a2850f1270a77559da35f993398b88364
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-01-02 02:10:10 +0800

    Update score.sh

--- score.sh
+++ score.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Configuration
-HW=6
+HW=11
 # -------------
 
-if test -d ~/Homework; then rm -r ~/Homework; fi 
+if test -d ~/Homework; then rm -rf ~/Homework; fi
 mkdir ~/Homework
 
 score_file=~/Homework/HW${HW}_Score
@@ -14,13 +14,15 @@ students_id=$(cat ~/students_id.txt)
 if (( $# != 0 )); then students_id=$@; fi
 
 for student_id in $students_id; do
-    mkdir ~/Homework/$student_id
-    cp ~/Submission_BackUp/$student_id/HW${HW}/hw${HW}.c ~/Homework/$student_id
-    # echo "hw$HW -p ~/Homework/$student_id"
-    hw$HW -p ~/Homework/$student_id > /dev/null 2>&1
-	score=$?
+    score=0
+    if test -f ~/Submission_BackUp/$student_id/HW${HW}/hw${HW}.c; then
+        mkdir ~/Homework/$student_id
+        cp ~/Submission_BackUp/$student_id/HW${HW}/hw${HW}.c ~/Homework/$student_id
+        hw$HW -p ~/Homework/$student_id > /dev/null 2>&1
+        score=$?
+    	echo "$student_id, $score"
+    fi
     echo "$student_id, $score" >> $score_file
-    echo "$student_id, $score"
 done
 
 if (( $# == 0 )); then ~/Moss/moss.sh; fi
