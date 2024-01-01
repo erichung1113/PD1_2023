ea07303f27aa83baacfc43823099ebdd5c5217af
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-01-02 02:19:28 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ BackUpDir=/home/cial1/Submission_BackUp
 if test -d $BackUpDir; then rm -r $BackUpDir; fi
 mkdir $BackUpDir
 
-for id in $(cat /home/cial1/students_id.txt); do
-	if test -d "/home/$id/HW$HW"; then 
+for id in $(cat /home/cial1/all_students_id.txt); do
+	if test -d /home/$id/HW$HW; then 
 		echo "copying $id" 
 		mkdir $BackUpDir/$id
 		cp -r /home/$id/HW$HW $BackUpDir/$id;
