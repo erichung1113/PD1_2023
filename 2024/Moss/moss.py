d7d2073f922e8d417bd350c2b2edac4ca4411808
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-10-31 22:25:26 +0800

    Update moss.py

--- 2024/Moss/moss.py
+++ 2024/Moss/moss.py
@@ -4,9 +4,12 @@ userid = 61861013
 for i in range(1, len(sys.argv)):
     problem = sys.argv[i]
     print("Uploading " + problem + " ......")
-    
+
     m = mosspy.Moss(userid, "python")
-    m.addFilesByWildcard(f"Submissions/${problem}/*")
+    m.addFilesByWildcard(f"Submissions/{problem}/*")
 
     url = m.send()
-    print (url
+    print (url)
+    
+    with open("plagiarism_result", "a") as file:
+        file.writelines(url + '\n')
