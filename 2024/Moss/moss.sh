b406114f20e50b373b90d2ab72655b357b15d2aa
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-10-31 19:48:52 +0800

    Update moss.sh

--- 2024/Moss/moss.sh
+++ 2024/Moss/moss.sh
@@ -1,25 +1,30 @@
-HW=$(ls /usr/local/bin | grep -oP '^hw\K[0-9]+' | sort -n | tail -1)
+#HW=$(ls /usr/local/bin | grep -oP '^hw\K[0-9]+' | sort -n | tail -1)
+HW=3
 
 function upload_to_Moss {
-	if test -d /home/cial1/Moss/Submissions; then rm -r /home/cial1/Moss/Submissions; fi
-	mkdir /home/cial1/Moss/Submissions
+    FILE=$1
+
+	File_Dir=$Submission_Dir/$FILE
+	if test -d $File_Dir; then rm -r $File_Dir; fi
+	mkdir $File_Dir
 
 	for id in $(cat /home/cial1/all_students_id.txt); do
-		for FILE in $@; do
-			if test -f /home/cial1/Submission_BackUp/${id}/HW${HW}/${FILE}; then
-				cp /home/cial1/Submission_BackUp/${id}/HW${HW}/${FILE} /home/cial1/Moss/Submissions/${id}_${FILE}
-			fi
-		done
+		if test -f /home/cial1/Submission_BackUp/${id}/HW${HW}/$FILE.c; then
+			cp /home/cial1/Submission_BackUp/${id}/HW${HW}/$FILE.c $File_Dir/${id}.c
+		fi
 	done
-	echo "uploading $@ to Moss...."
-	result=$(python3 /home/cial1/Moss/moss.py)
-	echo $result
-	echo $result >> /home/cial1/Moss/plagiarism_result
 }
 
-# change here
-upload_to_Moss "pA.c"
-upload_to_Moss "pB.c"
-upload_to_Moss "pC.c"
-upload_to_Moss "pD.c"
-upload_to_Moss "pE.c"
+Submission_Dir=/home/cial1/Moss/Submissions
+if test -d $Submission_Dir; then rm -r $Submission_Dir; fi
+mkdir $Submission_Dir
+
+problem=$(ls /share/HW${HW}_TC)
+for p in $problem; do
+	upload_to_Moss $p
+done
+
+cd /home/cial1/Moss
+result=$(python3 /home/cial1/Moss/moss.py $problem)
+echo $result
+echo $result >> /home/cial1/Moss/plagiarism_result
